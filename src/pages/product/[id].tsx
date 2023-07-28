b6e51f89328b71cc5fd8c8af0ad0a649aa58c9f2
Gabriel02ps <gabriel02ps@gmail.com>
2023-07-28 00:02:32 -0300

    chore: add product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          tempora delectus culpa pariatur vitae? Totam enim consectetur non
+          atque nesciunt deleniti qui, ex distinctio asperiores voluptatem
+          placeat, provident consequuntur pariatur?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
